4f79a275a36ba29ec43d82461001297e0e56da4c
Goorly <vlad.pigulevsky@gmail.com>
2020-07-22 09:21:03 +0300

    create backend by title. add some metadata.

---
+++ backend/src/main/java/com/test_task_resliv/tourist_hint_bot/TouristHintBotApplication.java
@@ -0,0 +1,13 @@
+package com.test_task_resliv.tourist_hint_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristHintBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouristHintBotApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/test_task_resliv/touristhintbot/TouristHintBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test_task_resliv.touristhintbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristHintBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
